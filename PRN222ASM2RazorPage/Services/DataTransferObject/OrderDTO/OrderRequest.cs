38be19882f5ec91e91947476e1f7df7b15343353
ThangDVSE183732 <thangdvse183732@fpt.edu.vn>
2025-10-18 18:53:43 +0700

    Create: CreateOrder method

---
+++ PRN222ASM2RazorPage/Services/DataTransferObject/OrderDTO/OrderRequest.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.DataTransferObject.OrderDTO
+{
+    public class CreateOrderRequest
+    {
+        [Required(ErrorMessage = "Customer ID is required")]
+        public int CustomerId { get; set; }
+
+        [Required(ErrorMessage = "Vehicle ID is required")]
+        public int VehicleId { get; set; }
+
+        [Required(ErrorMessage = "Amount is required")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than 0")]
+        public decimal Amount { get; set; }
+
+        public int? DealerId { get; set; }
+        public string? Notes { get; set; }
+    }
+
+    public class UpdateOrderStatusRequest
+    {
+        [Required(ErrorMessage = "Order ID is required")]
+        public int OrderId { get; set; }
+
+        [Required(ErrorMessage = "Status is required")]
+        [StringLength(50, ErrorMessage = "Status cannot exceed 50 characters")]
+        public string Status { get; set; } = string.Empty;
+
+        public string? Notes { get; set; }
+    }
+}
